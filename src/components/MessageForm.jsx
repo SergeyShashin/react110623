1a2c566f05ca87729df7ce335b117f72f6411bdd
SergeyShashin <s9997910@yandex.ru>
2023-07-02 00:25:49 +0300

    Add componnents Messenger, MessagesList, Message.

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -2,11 +2,30 @@ import React, { Component } from 'react';
 
 export class MessageForm extends Component {
 
-  
+  state = {
+    inputMessage: null
+  }
+
+  handlerInput = (event) => {
+    this.setState({
+      inputMessage: event.target.value
+    });
+  }
+
+  handlerButtonSend = () => {
+    this.props.getInputMessageFunction(this.state.inputMessage);
+    this.setState({
+      inputMessage: null
+    });
+  }
+
   render() {
     return (
       <div>
-       Текст
+        <label>
+          <input type="text" onChange={this.handlerInput} />
+        </label>
+        <button onClick={this.handlerButtonSend}>Отправить</button>
       </div >
     );
   }
