a5755075ec3e28e6b702a9e7a02e1a1bf6251c43
SergeyShashin <s9997910@yandex.ru>
2023-07-01 23:50:00 +0300

    Updated 01.07.23

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-<<<<<<<<< Temporary merge branch 1
-<<<<<<< HEAD
-import {App} from './components/App.jsx';
-=======
->>>>>>> ff73646813d13c0b786d49206f5bcf5e1e960f66
-=========
-import {App} from 'components/App';
->>>>>>>>> Temporary merge branch 2
-
-const messages = ['Привет!', 'Как дела?', 'Как погода?'];
-
-
+import {Messenger} from './components/Messenger';
 
 ReactDom.render(
-<<<<<<<<< Temporary merge branch 1
-<<<<<<< HEAD
-  <App />,
-=======
-  <MessageList messages={messages} />,
->>>>>>> ff73646813d13c0b786d49206f5bcf5e1e960f66
-=========
-  <App />,
->>>>>>>>> Temporary merge branch 2
+  <Messenger />,
   document.getElementById('root')
 );
